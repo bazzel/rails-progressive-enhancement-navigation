c8b9f1f46cf90fa65ad6c68146ec3c3086c1b1ec
Patrick Baselier <patrick.baselier@gmail.com>
2020-09-07 14:52:29 +0200

    Add live search

---
+++ app/javascript/controllers/search_controller.js
@@ -0,0 +1,19 @@
+import _ from "lodash";
+import { Controller } from "stimulus";
+import Rails from "@rails/ujs";
+
+export default class extends Controller {
+  static targets = ["form", "query", "submit"];
+
+  initialize() {
+    this.submitTarget.remove();
+  }
+  connect() {
+    this.perform = _.debounce(this._perform, 500).bind(this);
+  }
+
+  _perform(event) {
+    event.preventDefault();
+    Rails.fire(this.formTarget, "submit");
+  }
+}
