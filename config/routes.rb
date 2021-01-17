5626359d9173f745693105433cbb7d8546c39557
Patrick Baselier <patrick.baselier@gmail.com>
2021-01-17 11:23:30 +0100

    Replace `root`

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'companies/:id', to: 'companies#show', as: 'example'
+  get '/', to: redirect('/companies/1')
   get 'snippets/:id', to: 'snippets#show', as: 'snippet'
-
-  root 'companies#1'
 end
