4f814cf1e5678d2f4fad289c319cd327bc94d8d8
Patrick Baselier <patrick.baselier@gmail.com>
2021-01-17 14:29:03 +0100

    Show diff

--- app/components/code_snippet_component.rb
+++ app/components/code_snippet_component.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class CodeSnippetComponent < ViewComponent::Base
+  SNIPPETS_ROOT = 'docs/snippets'
+
   attr_reader :sample_file
+
+  delegate :basename, to: :sample_file
 
   def initialize(sample_file:)
     @sample_file = sample_file
@@ -9,11 +13,20 @@ class CodeSnippetComponent < ViewComponent::Base
 
   private
 
-  def title
-    File.basename(sample_file)
-  end
+  alias title basename
 
   def snippet
     CodeRay.highlight_file(sample_file, line_numbers: :table).html_safe
+  end
+
+  def previous_sample_files
+    current_id = sample_file.dirname.basename.to_s.to_i
+    previous_ids = (1..(current_id - 1))
+
+    previous_ids.map do |id|
+      next unless (file = Rails.root.join(SNIPPETS_ROOT, id.to_s, basename)).exist?
+
+      file
+    end.compact
   end
 end
